009dbaf2533b8bc4bd76dd0a1b433957a87e8d30
Michael Gargiullo <mgargiullo@pvtpt.com>
2018-01-11 10:18:42 -0500

    Update AppSpider to match current NTOSpider plugin

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -1,5 +1,5 @@
-class APPSpiderTasks < Thor
-  include Core::Pro::ProjectScopedTask if defined?(::Core::Pro)
+class NTOSpiderTasks < Thor
+  include Rails.application.config.dradis.thor_helper_module
 
   namespace "dradis:plugins:appspider"
 
@@ -7,33 +7,14 @@ class APPSpiderTasks < Thor
   def upload(file_path)
     require 'config/environment'
 
-    logger = Logger.new(STDOUT)
-    logger.level = Logger::DEBUG
-
     unless File.exists?(file_path)
       $stderr.puts "** the file [#{file_path}] does not exist"
       exit -1
     end
 
-    content_service = nil
-    template_service = nil
-    if defined?(Dradis::Pro)
-      detect_and_set_project_scope
-      content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::APPSpider)
-      template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::APPSpider)
-    else
-      content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::APPSpider)
-      template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::APPSpider)
-    end
+    detect_and_set_project_scope
 
-    importer = Dradis::Plugins::APPSpider::Importer.new(
-                logger: logger,
-       content_service: content_service,
-      template_service: template_service
-    )
-
+    importer = Dradis::Plugins::APPSpider::Importer.new(task_options)
     importer.import(file: file_path)
-
-    logger.close
   end
 end
