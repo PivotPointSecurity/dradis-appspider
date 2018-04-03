ce0fc53f849f53252d4b9149273577a0229221c8
root <root@dradisstaging.local>
2018-04-03 14:35:58 -0400

    update version

--- lib/dradis/plugins/appspider/gem_version.rb
+++ lib/dradis/plugins/appspider/gem_version.rb
@@ -9,7 +9,7 @@ module Dradis
       module VERSION
         MAJOR = 2
         MINOR = 0
-        TINY = 1
+        TINY = 4
         PRE = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
